ec92851d256cb0775324f6a84ae238d8fa0e1513
Hong Seung-Gi <tmdrl5661@naver.com>
2021-01-16 14:41:07 +0900

    [Update] react-bootstrap 적용한 메인 페이지 코드 업데이트

--- QuickChat_v1/src/App.js
+++ QuickChat_v1/src/App.js
@@ -1,24 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import {Navbar, NavDropdown, Nav, MenueItem} from 'react-bootstrap';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+				<Navbar collapseOnSelect expand="lg" className="top-nav">
+          <div className="container">
+          <Navbar.Brand href="#home">Quick Chat</Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav" className="menu-list">
+            <Nav >
+              <Nav.Link href="#About">About</Nav.Link>
+              <Nav.Link href="#Login">Login</Nav.Link>
+              <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                <NavDropdown.Item href="">Another action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                <NavDropdown.Divider />
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+          </div>
+        </Navbar>
+			</div>
   );
 }
 
