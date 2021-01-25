23bcd5ee63598b76fe9aebab6b908bffff5eed9f
Hong Seung-Gi <tmdrl5661@naver.com>
2021-01-25 17:52:31 +0900

    [Update] 작성된 Login.js를 import 하여 modal 기능으로 해당 스크립트를 팝업 하는 기능 업데이트

--- QuickChat_v1/src/App.js
+++ QuickChat_v1/src/App.js
@@ -1,33 +1,51 @@
 import logo from './logo.svg';
 import React, {Component} from 'react';
 import './App.css';
+import Login from "./Login/Login";
 import {Navbar, NavDropdown, Nav, MenueItem} from 'react-bootstrap';
 
  class App extends Component {
-  render(){
-        return (
-           <div>
+      constructor(props) {
+        super(props);
+        this.state = {
+          isModalOpen: false,
+        };
+      }
+
+      openModal = () => {
+        this.setState({ isModalOpen: true });
+      };
+
+      closeModal = () => {
+        this.setState({ isModalOpen: false });
+      };
+      render(){
+            return (
+              <div>
                 <Navbar collapseOnSelect expand="lg" className="top-nav">
                   <div className="container">
-                  <Navbar.Brand href="#home">Quick Chat</Navbar.Brand>
-                  <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                  <Navbar.Collapse id="responsive-navbar-nav" className="menu-list">
-                    <Nav >
-                      <Nav.Link href="#About">About</Nav.Link>
-                      <Nav.Link href="#Login">Login</Nav.Link>
-                      <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-                        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                        <NavDropdown.Item href="">Another action</NavDropdown.Item>
-                        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                        <NavDropdown.Divider />
-                      </NavDropdown>
-                    </Nav>
-                  </Navbar.Collapse>
+                    <Navbar.Brand href="#home">Quick Chat</Navbar.Brand>
+                      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                        <Navbar.Collapse id="responsive-navbar-nav" className="menu-list">
+                          <Nav >
+                            <Nav.Link href="#About">About</Nav.Link>
+                            <Nav.Link onClick = {this.openModal}>Login</Nav.Link>
+                            <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+                              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                              <NavDropdown.Item href="">Another action</NavDropdown.Item>
+                              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                              <NavDropdown.Divider />
+                            </NavDropdown>
+                          </Nav>
+                        </Navbar.Collapse>
                   </div>
                 </Navbar>
-           </div>
-        );
-  }
+                <div>
+                  <Login isOpen={this.state.isModalOpen} close={this.closeModal} />   
+                </div>       
+              </div>
+            );
+      }
 }
 
 export default App;
